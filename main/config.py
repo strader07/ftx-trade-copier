9cf41fe54053d1f40d57bbf54c2a13f61e5748d4
rvision0925 <huniu199531@gmail.com>
2021-01-27 19:45:46 -0600

    v2 multiple sub accounts

--- main/config.py
+++ main/config.py
@@ -5,7 +5,21 @@ load_dotenv()
 MASTER_API = os.environ.get('MASTER_API')
 MASTER_SECRET = os.environ.get('MASTER_SECRET')
 
-SLAVE_API = os.environ.get('SLAVE_API')
-SLAVE_SECRET = os.environ.get('SLAVE_SECRET')
+try:
+    NUM_SLAVES = int(os.environ.get('NUM_SLAVES'))
+except:
+    NUM_SLAVES = 1
 
-LEVERAGE = os.environ.get('LEVERAGE')
+if NUM_SLAVES < 1:
+    NUM_SLAVES = 1
+
+SLAVE_APIS = []
+SLAVE_SECRETS = []
+SLAVE_NAMES = []
+for i in range(NUM_SLAVES):
+    SLAVE_APIS.append(os.environ.get(f'SLAVE_API{i+1}'))
+    SLAVE_SECRETS.append(os.environ.get(f'SLAVE_SECRET{i+1}'))
+    SLAVE_NAMES.append(os.environ.get(f'SLAVE_SUBACC_NAME{i+1}'))
+
+MASTER_LEVERAGE = int(os.environ.get("MASTER_LEVERAGE"))
+SLAVE_LEVERAGE = int(os.environ.get("SLAVE_LEVERAGE"))
--- main/views.py
+++ main/views.py
@@ -8,12 +8,18 @@ from accounts.models import BotSetting
 from threading import Thread
 from datetime import datetime
 import time
+import os
 
 from main.ftx.api import FtxClient
-from .config import MASTER_API, MASTER_SECRET, SLAVE_API, SLAVE_SECRET
+from .config import *
 
-client1 = FtxClient(api_key=MASTER_API, api_secret=MASTER_SECRET)
-client2 = FtxClient(api_key=SLAVE_API, api_secret=SLAVE_SECRET, subaccount_name="Trader")
+main_client = FtxClient(api_key=MASTER_API, api_secret=MASTER_SECRET)
+sub_clients = []
+for i in range(NUM_SLAVES):
+    client = FtxClient(api_key=SLAVE_APIS[i], api_secret=SLAVE_SECRETS[i], subaccount_name=SLAVE_NAMES[i])
+    sub_clients.append(client)
+mLeverage = MASTER_LEVERAGE
+sLeverage = SLAVE_LEVERAGE
 
 
 def index(request, template_name="main/index.html"):
@@ -43,55 +49,68 @@ def check_bot_setting(user):
         return False
 
 
-def place_order(order):
+def place_order(order, mBalance):
     print(order)
     try:
         market = order["market"]
         price = order["price"]
         side = order["side"]
-        size = order["size"]
+        _size = order["size"]
         _type = order["type"]
     except Exception as e:
         print(e)
         return None
 
-    try:
-        res = client2.place_order(market, side, None, size, _type)
-    except Exception as e:
-        print(e)
-        res = {"success":False}
+    order_results = []
+    for client in sub_clients:
+        sBalance = client.get_account_info()["freeCollateral"]
+        size = round(_size * (sBalance / mBalance) * (sLeverage / mLeverage), 4)
+        if size == 0:
+            size = _size
 
-    return res
+        try:
+            res = client.place_order(market, side, price, size, _type)
+            order_results.append({"success":True, "result": res})
+        except Exception as e:
+            print(e)
+            res = {"success":False, "result": e}
+            order_results.append(res)
+
+    return order_results
 
 
 def run_bot(user):
     loop = check_bot_setting(user)
     try:
-        last_orders = client1.get_order_history()
+        last_orders = main_client.get_order_history()
     except Exception as e:
         print(e)
         last_orders = []
     last_orders = [order for order in last_orders if order["avgFillPrice"] or order["status"]!="closed"]
-    print(last_orders)
+    print(len(last_orders))
+
+    mBalance = main_client.get_account_info()["freeCollateral"]
 
     while loop:
         print(datetime.now(), " ========")
         try:
-            new_orders = client1.get_order_history()
+            new_orders = main_client.get_order_history()
         except Exception as e:
             new_orders = []
         new_orders = [order for order in new_orders if order["avgFillPrice"] or order["status"]!="closed"]
-
+        print(len(new_orders))
         orders = [order for order in new_orders if order not in last_orders]
         last_orders = new_orders
         print(orders)
 
         for order in orders:
-            res = place_order(order)
-            print("order success")
-            print(res)
+            results = place_order(order, mBalance)
+            print("order status!")
+            for res, i in zip(results, range(len(results))):
+                print(f"{i+1}-th account: \n", res)
 
         time.sleep(10)
+        bal1 = main_client.get_account_info()["freeCollateral"]
         loop = check_bot_setting(user)
 
 
